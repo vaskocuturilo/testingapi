f77c948a1fbee71ea805ff23123d1d99f1c33764
Vasko <vaskocuturilo@rambler.ru`>
2019-11-27 13:07:41 +0300

    Feat : Data : Added JavaDoc.
    Commit at 27/11/2019

--- src/main/java/com/wargaming/api/schema/info/Data.java
+++ src/main/java/com/wargaming/api/schema/info/Data.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.annotation.Generated;
 
+
+/**
+ * The class Data.
+ */
 @SuppressWarnings({"PMD.MethodArgumentCouldBeFinal", "PMD.ShortVariable", "PMD.LongVariable", "PMD.ShortClassName"})
 @Generated("com.robohorse.robopojogenerator")
 public class Data {
@@ -11,10 +15,20 @@ public class Data {
     @JsonProperty("6269")
     private JsonMember6269 jsonMember6269;
 
+    /**
+     * Sets json member 6269.
+     *
+     * @param jsonMember6269 the json member 6269
+     */
     public void setJsonMember6269(JsonMember6269 jsonMember6269) {
         this.jsonMember6269 = jsonMember6269;
     }
 
+    /**
+     * Gets json member 6269.
+     *
+     * @return the json member 6269
+     */
     public JsonMember6269 getJsonMember6269() {
         return jsonMember6269;
     }
