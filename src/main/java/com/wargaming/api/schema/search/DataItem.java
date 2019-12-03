c5ccdbe31a6de24eb7c02caf9e9bc4e6b9c46c29
Vasko <vaskocuturilo@rambler.ru`>
2019-12-03 13:18:55 +0300

    Feat : DataItem : Added JavaDoc.
    Commit at 03/12/2019

--- src/main/java/com/wargaming/api/schema/search/DataItem.java
+++ src/main/java/com/wargaming/api/schema/search/DataItem.java
@@ -1,40 +1,64 @@
 package com.wargaming.api.schema.search;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.annotation.Generated;
 
+/**
+ * The class Data item.
+ */
 @SuppressWarnings("PMD.MethodArgumentCouldBeFinal")
 @Generated("com.robohorse.robopojogenerator")
-public class DataItem{
+public class DataItem {
 
-	@JsonProperty("account_id")
-	private int accountId;
+    @JsonProperty("account_id")
+    private int accountId;
 
-	@JsonProperty("nickname")
-	private String nickname;
+    @JsonProperty("nickname")
+    private String nickname;
 
-	public void setAccountId(int accountId){
-		this.accountId = accountId;
-	}
+    /**
+     * Sets account id.
+     *
+     * @param accountId the account id
+     */
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
 
-	public int getAccountId(){
-		return accountId;
-	}
+    /**
+     * Gets account id.
+     *
+     * @return the account id
+     */
+    public int getAccountId() {
+        return accountId;
+    }
 
-	public void setNickname(String nickname){
-		this.nickname = nickname;
-	}
+    /**
+     * Sets nickname.
+     *
+     * @param nickname the nickname
+     */
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 
-	public String getNickname(){
-		return nickname;
-	}
+    /**
+     * Gets nickname.
+     *
+     * @return the nickname
+     */
+    public String getNickname() {
+        return nickname;
+    }
 
-	@Override
- 	public String toString(){
-		return 
-			"DataItem{" + 
-			"account_id = '" + accountId + '\'' + 
-			",nickname = '" + nickname + '\'' + 
-			"}";
-		}
+    @Override
+    public String toString() {
+        return
+                "DataItem{" +
+                        "account_id = '" + accountId + '\'' +
+                        ",nickname = '" + nickname + '\'' +
+                        "}";
+    }
 }
