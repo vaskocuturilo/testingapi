3fc5cb6bb4aa9e140b45524c200bd7807a6ac476
Vasko <vaskocuturilo@rambler.ru`>
2019-12-18 16:22:23 +0300

    Feat : Error : Added JavaDoc.
    Commit at 18/12/2019

--- src/main/java/com/wargaming/api/schema/noaccountid/Error.java
+++ src/main/java/com/wargaming/api/schema/noaccountid/Error.java
@@ -1,64 +1,109 @@
 package com.wargaming.api.schema.noaccountid;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.annotation.Generated;
 
+
+/**
+ * The class Error.
+ */
 @SuppressWarnings("PMD.MethodArgumentCouldBeFinal")
 @Generated("com.robohorse.robopojogenerator")
-public class Error{
+public class Error {
 
-	@JsonProperty("code")
-	private int code;
+    @JsonProperty("code")
+    private int code;
 
-	@JsonProperty("field")
-	private String field;
+    @JsonProperty("field")
+    private String field;
 
-	@JsonProperty("message")
-	private String message;
+    @JsonProperty("message")
+    private String message;
 
-	@JsonProperty("value")
-	private Object value;
+    @JsonProperty("value")
+    private Object value;
 
-	public void setCode(int code){
-		this.code = code;
-	}
+    /**
+     * Set code.
+     *
+     * @param code the code
+     */
+    public void setCode(int code) {
+        this.code = code;
+    }
 
-	public int getCode(){
-		return code;
-	}
+    /**
+     * Get code int.
+     *
+     * @return the int
+     */
+    public int getCode() {
+        return code;
+    }
 
-	public void setField(String field){
-		this.field = field;
-	}
+    /**
+     * Set field.
+     *
+     * @param field the field
+     */
+    public void setField(String field) {
+        this.field = field;
+    }
 
-	public String getField(){
-		return field;
-	}
+    /**
+     * Get field string.
+     *
+     * @return the string
+     */
+    public String getField() {
+        return field;
+    }
 
-	public void setMessage(String message){
-		this.message = message;
-	}
+    /**
+     * Set message.
+     *
+     * @param message the message
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
-	public String getMessage(){
-		return message;
-	}
+    /**
+     * Get message string.
+     *
+     * @return the string
+     */
+    public String getMessage() {
+        return message;
+    }
 
-	public void setValue(Object value){
-		this.value = value;
-	}
+    /**
+     * Set value.
+     *
+     * @param value the value
+     */
+    public void setValue(Object value) {
+        this.value = value;
+    }
 
-	public Object getValue(){
-		return value;
-	}
+    /**
+     * Get value object.
+     *
+     * @return the object
+     */
+    public Object getValue() {
+        return value;
+    }
 
-	@Override
- 	public String toString(){
-		return 
-			"Error{" + 
-			"code = '" + code + '\'' + 
-			",field = '" + field + '\'' + 
-			",message = '" + message + '\'' + 
-			",value = '" + value + '\'' + 
-			"}";
-		}
+    @Override
+    public String toString() {
+        return
+                "Error{" +
+                        "code = '" + code + '\'' +
+                        ",field = '" + field + '\'' +
+                        ",message = '" + message + '\'' +
+                        ",value = '" + value + '\'' +
+                        "}";
+    }
 }
